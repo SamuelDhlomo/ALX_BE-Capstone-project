ea4c37fec995601d6cc9be7b8eed551d142d2db7
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-05 19:03:19 +0200

    Updated models.py to define models for User, Movie, Review

--- movie_review_api/movies/models.py
+++ movie_review_api/movies/models.py
@@ -1,8 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=('groups'),
+        blank=True,
+        help_text=(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="movie_app_users_groups",  # Unique related_name
+        related_query_name="movie_app_user_group", # Unique related_query_name
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=('user permissions'),
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        related_name="movie_app_users_permissions", # Unique related_name
+        related_query_name="movie_app_user_permission", # Unique related_query_name
+    )
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -12,5 +31,5 @@ class Movie(models.Model):
 class Review(models.Model):
     text = models.TextField()
     rating = models.IntegerField()
-    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
