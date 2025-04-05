5eb2d7cf0ec7e834d28c627196d2acfe2d82e2da
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-05 18:40:20 +0200

    Defined models for User, Movie, Review

--- movie_review_api/movies/models.py
+++ movie_review_api/movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ChaeField(max_length=255)
+    release_date = models.DateField()
+
+class Review(models.Model):
+    text = models.TextField()
+    rating = models.IntegerField()
+    movie = modles.ForeignKey(Movie,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
