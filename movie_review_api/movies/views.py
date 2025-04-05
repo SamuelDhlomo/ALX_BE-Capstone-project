8f38e61b19097c0014ff97bdb8da0705569d7c78
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-05 19:16:09 +0200

    Implemented API endpoints for movies and reviews

--- movie_review_api/movies/views.py
+++ movie_review_api/movies/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Movie, Review
+from .serializers import MovieSerializer, ReviewSerializer
 
-# Create your views here.
+class MovieView(APIView):
+def get(self, request):
+movies = Movie.objects.all()
+serializer = MovieSerializer(movies, many=True)
+return Response(serializer.data)
+
+def post(self, request):
+serializer = MovieSerializer(data=request.data)
+if serializer.is_valid():
+serializer.save()
+return Response(serializer.data, status=201)
+return Response(serializer.errors, status=400)
+
+class ReviewView(APIView):
+def get(self, request):
+reviews = Review.objects.all()
+serializer = ReviewSerializer(reviews, many=True)
+return Response(serializer.data)
+
+def post(self, request):
+serializer = ReviewSerializer(data=request.data)
+if serializer.is_valid():
+serializer.save()
+return Response(serializer.data, status=201)
+return Response(serializer.errors, status=400)
