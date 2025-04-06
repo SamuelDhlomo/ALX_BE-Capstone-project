8d4247e8fd1af44013100ab82b49b3d0e9ec4a70
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-06 23:51:01 +0200

    Add pagination and sorting to MovieListView and ReviewListView

--- movie_review_api/movies/views.py
+++ movie_review_api/movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Movie, Review
 from .serializers import MovieSerializer, ReviewSerializer
@@ -7,8 +7,14 @@ class MovieListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title', 'release_date']
+    ordering = ['title']
 
 class ReviewListView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['rating', 'created_at', 'movie__title']
+    ordering = ['-created_at']
