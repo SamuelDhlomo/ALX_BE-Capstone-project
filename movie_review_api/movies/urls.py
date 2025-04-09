1921a4b82f94238edf62d3a2a573429970f2525a
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-10 00:58:53 +0200

    Restructure repository root

--- movie_review_api/movies/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from django.urls import path
-from .views import MovieListView, ReviewListView, RootView
-
-urlpatterns = [
-    path('movies/', MovieListView.as_view(), name='movie-list'),
-    path('reviews/', ReviewListView.as_view(), name='review-list'),
-    path('', RootView.as_view(), name='root'),
-]
