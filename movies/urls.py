9843d903c8f1a778afa1f55acb62d64a709cfd4c
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-10 21:44:35 +0200

    updated urls.py and views.py to define ReviewList

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MovieListView
+from .views import MovieListView, ReviewListView, ReviewDetailView  # Make sure to import ReviewListView and ReviewDetailView
 
 urlpatterns = [
     path('movies/', MovieListView.as_view(), name='movie-list'),
-
+    path('reviews/', ReviewListView.as_view(), name='review-list'),
+    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -3,19 +3,35 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination # Import pagination
-from .models import Movie
-from .serializers import MovieSerializer
-from .filters import MovieFilter # Import your MovieFilter
+from .models import Movie, Review  # Make sure to import the Review model
+from .serializers import MovieSerializer, ReviewSerializer
+from .filters import MovieFilter # Assuming you have MovieFilter
 
 class MoviePagination(PageNumberPagination):
-    page_size = 10 # Set the number of movies per page
+    page_size = 10
 
 class MovieListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter] # Add DjangoFilterBackend
-    filterset_class = MovieFilter # Specify the filter set for movies
-    ordering_fields = ['title', 'release_date', 'rating_avg'] # Allow ordering
-    ordering = ['title'] # Default ordering
-    pagination_class = MoviePagination # Enable pagination for movies
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = MovieFilter
+    ordering_fields = ['title', 'release_date', 'rating_avg']
+    ordering = ['title']
+    pagination_class = MoviePagination
+
+class ReviewPagination(PageNumberPagination):
+    page_size = 15
+
+class ReviewListView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user) # Automatically set the user
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
