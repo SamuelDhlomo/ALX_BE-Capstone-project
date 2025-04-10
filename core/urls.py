47d5deafba80d530f32ff66307b9a5a3a4aca003
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-10 21:35:51 +0200

    fixed syntax error in urls

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('movies.urls')),
-    path('', views.home, name='home'),   
+    path('', views.home, name='home'),  
+]
