e628364ccdfbb768648020595af7f64044b22d94
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-10 21:33:11 +0200

    updated root urls to define reviews

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('movies.urls')),
-    path('', views.home, name='home'),
-]
+    path('', views.home, name='home'),   
