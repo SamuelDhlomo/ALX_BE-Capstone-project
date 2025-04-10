08f9a2a2264ba723a6b01cba4ed30536e3847bad
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-10 21:18:59 +0200

    updated urls to define path

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import MovieListView, ReviewListView
+from .views import MovieListView
 
 urlpatterns = [
     path('movies/', MovieListView.as_view(), name='movie-list'),
-    path('reviews/', ReviewListView.as_view(), name='review-list'),
+
 ]
